67876a2fe87561503827bf25465fb1979d19360c
Mayank <mayankbaoniya9@gmail.com>
2026-01-09 18:31:43 +0530

    feat: add safe-buffer and uid-safe modules.

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,35 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./db.js";
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+
+declare module 'express-session' {
+  interface SessionData {
+    isLoggedIn: boolean;
+    userId: string;
+  }
+}
 
 await connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  credentials: true,
+}));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET as string,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}, // 7 days
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI as string,
+    collectionName: 'sessions'
+  })
+}))
 
 app.use(express.json());
 
