8c970b8d7c6b9106a5490479774714760c26e90c
Mayank <mayankbaoniya9@gmail.com>
2026-01-09 17:00:31 +0530

    Some important packages and dependencies are implemented in server folder.

---
+++ server/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./db.js";
+
+await connectDB();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
